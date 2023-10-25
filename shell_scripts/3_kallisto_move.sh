1a48e36445c7a97bf6c41dd243b9333893685b7d
mandysulli <89869003+mandysulli@users.noreply.github.com>
2023-10-25 14:10:00 -0400

    Create 3_kallisto_move.sh

---
+++ shell_scripts/3_kallisto_move.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=Kallisto_quant_psuedobam
+#SBATCH --partition=partition
+#SBATCH --ntasks=1
+#SBATCH --mem=#
+#SBATCH --export=
+#SBATCH --time=##:##:##
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=$$$$$$@email.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+##Moving the tsv files and pseudobam files to one location
+##May need a fair amount of time for this. The bam files can be big.
+
+#Set directory
+cd /file/path/to/Kallisto_outputs
+
+mkdir Kallisto_tsv_files
+mkdir Kallisto_pseudobam_files
+
+echo "$i"
+mv ./Sample\/Sample_abundance.tsv ./Kallisto_tsv_files
+mv ./Sample\/Sample\_pseudoalignments.bam ./Kallisto_pseudobam_files
