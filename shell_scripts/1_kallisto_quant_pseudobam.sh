629a9f27a26b77208c2f6ddf4316165c9715fd73
mandysulli <89869003+mandysulli@users.noreply.github.com>
2023-10-25 13:52:15 -0400

    first shell script

---
+++ shell_scripts/1_kallisto_quant_pseudobam.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=Kallisto_quant_psuedobam
+#SBATCH --partition=partition
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=#
+#SBATCH --export=
+#SBATCH --time=##:##:##
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=$$$$$$@email.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load kallisto/0.48.0-gompi-2022a
+
+#Set directory
+cd /file/path/to/working/directory
+
+#define directory
+index='/file/path/to/indexed_ref_database/'
+input='/file/path/to//paired_trim_reads'
+output='/file/path/to/Kallisto_outputs'
+
+
+kallisto quant -t 6 -b 100 --pseudobam -i $index/sequences.kallisto_idx -o $output/Sample $input/Sample_pair_trim_R1.fastq.gz $input/Sample_pair_trim_R2.fastq.gz
