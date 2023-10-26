9a4233f9b5598a698a58c5a65a057572c6d07b3a
mandysulli <89869003+mandysulli@users.noreply.github.com>
2023-10-26 15:24:23 -0400

    lineage setup

---
+++ shell_scripts/4_lineage_file_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=Kallisto_quant_psuedobam
+#SBATCH --partition=partition
+#SBATCH --ntasks=1
+#SBATCH --mem=#
+#SBATCH --export=
+#SBATCH --time=##:##:##
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=$$$$$$@email.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load Java/13.0.2
+
+##Will need to make lineage folder with your strain_key.txt and lineage_file_setup.class file inside of it
+
+#Set directory to execute script
+cd /scratch/mandyh/WISER_MC_Kallisto_Paper/lineage_files
+
+java lineage_file_setup strain_key.txt
